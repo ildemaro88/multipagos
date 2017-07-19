ebef39b8e4dead405502654e12a81ee5378a2045
Ildemaro en Recargas <ildemaro88@gmail.com>
2017-07-19 21:09:36 +0000

    configuration

--- public/js/configServer.js
+++ public/js/configServer.js
@@ -1,2 +1,2 @@
-var URL_BASE='http://52.173.244.34/recargas/public/admin/';
+var URL_BASE='http://34.212.4.215/recargas/public/admin/';
 //var URL_BASE='http://54.190.32.238/MedicSystem/public/admin/';
